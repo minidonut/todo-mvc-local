75938b8053e77f6817c7fe3bff05afe9d94663c9
minidonut <nycom13@gmail.com>
2020-04-25 03:23:32 +0900

    Redirect from / to /todos/all

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const StyledContainer = styled.div`
   max-width: 550px;
@@ -8,6 +9,14 @@ const StyledContainer = styled.div`
 
 
 export const Container = (props) => {
+  const history = useHistory();
+
+  React.useEffect(() => {
+    if (history.location.pathname === "/") {
+      history.push("/todos/all");
+    }
+  }, [history]);
+
   return <StyledContainer>
     {props.children}
   </StyledContainer>;
